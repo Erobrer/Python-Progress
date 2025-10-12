ef89ad46e2257e99abc4a58e4a50ebf79253ca17
Erobrer <fatihakbiyik5@gmail.com>
2025-10-12 17:22:19 +0300

    First Steps
    Hello World, Variables, Math, User Input

---
+++ first_steps.py
@@ -0,0 +1,58 @@
+# print("Hello World")
+#
+#
+# print("------------------------------")
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+# print("-------------------")
+
+
+# character_name = "Fatih"
+# character_age = 19
+# print("------------------------------")
+# print(f"There was a guy called " + character_name + ",")
+# print("He was " + str(character_age) + " years old.")
+# print(f"There was a guy called {character_name}, {character_age} years old.")
+# print("-----------------------------")
+
+# name = "Fatih"
+# print("------------------------------")
+# print("Fatih\nAkbiyik")
+# print(name + "is a good name")
+# print(name.upper())
+# print(name.lower())
+# print(name.islower())
+# print(len(name))
+# print(name[0])
+# print(name.index("F"))
+# print(name.replace("Fatih", "Talebe"))
+# print("------------------------------")
+
+# print("------------------------------")
+# from math import *
+# print(2)
+# print(2.5245)
+# print(2+8.5)
+# print(2 * (9 + 7))
+# print(10 % 3)
+# my_number = -10
+# print(str(my_number)) #Converts an integer to a string
+# print(abs(my_number))
+# print(pow(my_number, 2)) #-10^2
+# print(max(my_number, 9, 19)) #Compares numbers
+# print(min(my_number, 9, 19))
+# print(round(3.2))
+# print(round(3.7))
+# print(floor(3.7))
+# print(ceil(3.7))
+# print(sqrt(225))
+# print(sqrt(2)*sqrt(2))
+# print("------------------------------")
+
+print("------------------------------")
+user_name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("Hello " + user_name + "! You are " + str(age) + " years old.")
+print("------------------------------")
